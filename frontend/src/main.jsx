50d44f7f850c9d029aa4d947e60bf31b58a80b05
Shivam Sen <shivamsen9644@gmail.com>
2025-10-23 16:36:28 +0530

    ready to render deploy final deploy123

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,14 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { AuthProvider } from "./context/AuthProvider.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { SocketProvider } from "./context/SocketContext.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <AuthProvider>
       <SocketProvider>
         <App />
       </SocketProvider>
     </AuthProvider>
-  </BrowserRouter>
+  </HashRouter>
+  
 );
